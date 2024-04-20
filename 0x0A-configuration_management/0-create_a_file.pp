bfb7b796086f6093a4d4e32607286e7a218f5708
Dalhatmd <umarwalid4055@gmail.com>
2024-04-19 21:01:08 -0700

    small fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp/school' :
-ensure  => 'file',
-content => 'I love puppet',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
+#Creates a file in /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
