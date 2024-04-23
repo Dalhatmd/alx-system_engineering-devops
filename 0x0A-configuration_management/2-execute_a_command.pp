d14ab392564073078f0be578a4941b474f1440cd
Dalhatmd <umarwalid4055@gmail.com>
2024-04-23 11:44:07 +0100

    Fix puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process
 exec { 'kill':
-     command => 'pkill -f killmenow',
-     path    => ['/usr/bin', '/usr/sbin']
+    command => 'pkill -f killmenow',
+    path    => ['/usr/bin', '/usr/sbin']
 }
