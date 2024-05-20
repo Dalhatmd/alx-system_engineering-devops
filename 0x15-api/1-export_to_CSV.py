d52078698a487fd07a710f5c0b0292101a64b724
Dalhatmd <umarwalid4055@gmail.com>
2024-05-20 13:54:07 +0100

    Add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module that gather data from an API """
+import requests
+import json
+import sys
+import csv
+
+
+def gather_data():
+    """ retrieves data from an API """
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId\
+            ={sys.argv[1]}"
+    employee = requests.get(employee_url).json()
+    todos = requests.get(todo_url).json()
+    employee_name = employee.get("name")
+    completed = sum(1 for todo in todos if todo.get("completed"))
+    total = len(todos)
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for todo in todos:
+        if todo.get('completed'):
+            print(f'\t{todo["title"]}')
+    rows = [[employee_name, todo['title'], todo['title'], todo['completed']]
+            for todo in todos]
+    with open('USER_ID.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(rows)
+
+
+if __name__ == '__main__':
+    gather_data()
