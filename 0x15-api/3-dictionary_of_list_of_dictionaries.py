aff24a56a46abf669cb7c8be3e7d18000a2b7217
Dalhatmd <umarwalid4055@gmail.com>
2024-05-20 19:53:51 +0100

    Add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import json
+import requests
+
+
+def list_dict():
+    """ records all tasks from all employees """
+    employee_url = "https://jsonplaceholder.typicode.com/users"
+    employee = requests.get(employee_url).json()
+
+    dict = {}
+    for emp in employee:
+        user_id = emp.get("id")
+        emp_name = emp.get("username")
+        t_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+        todos = requests.get(t_url).json()
+
+        dict[user_id] = [{"username": emp_name, "task": todo["title"],
+                          "completed": todo["completed"], } for todo in todos]
+
+        with open('todo_all_employees.json', 'w') as f:
+            json.dump(dict, f)
+
+
+if __name__ == '__main__':
+    list_dict()
