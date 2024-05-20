67edaa9a307317ac0d511daef1c5334044ff46ef
Dalhatmd <umarwalid4055@gmail.com>
2024-05-20 19:21:46 +0100

    Fix task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Module that gather data from an API """
-import requests
+""" Module that gathers data from an API """
+import csv
 import json
+import requests
 import sys
 
 
 def gather_data():
-    """ retrieves data from an API """
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId\
-            ={sys.argv[1]}"
+    """ Retrieves data from an API """
+    user_id = sys.argv[1]
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+
     employee = requests.get(employee_url).json()
     todos = requests.get(todo_url).json()
-    employee_name = employee.get("name")
-    completed = sum(1 for todo in todos if todo.get("completed"))
-    total = len(todos)
-    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
-    for todo in todos:
-        if todo.get('completed'):
-            print(f'\t{todo["title"]}')
-    dict = {sys.argv[1]: [{"task": todo["title"], "cpmpleted": todo["completed"],
-                        "username": employee_name} for todo in todos]}
-    with open('USER_ID.json', 'w') as f:
+
+    emp_name = employee.get("username")
+
+    dict = {user_id: [{"task": todo["title"], "completed": todo["completed"],
+                      "username": emp_name} for todo in todos]}
+    with open(f'{user_id}.json', 'w') as f:
         json.dump(dict, f)
 
 
