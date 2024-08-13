dec608b93e62bfc310a7eb48b1cef7ec126f2529
Dalhatmd <umarwalid4055@gmail.com>
2024-08-13 17:12:42 +0100

    fix pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3 
-""" module that gets number of active users in a subreddit using the reddit API"""
+#!/usr/bin/python3
+""" module that gets number of subscribers in a subreddit"""
 import requests
 import json
+
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers in subreddit """
@@ -9,16 +10,14 @@ def number_of_subscribers(subreddit):
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-
     try:
-        response = requests.get(url, headers=header, allow_redirects = False)
+        response = requests.get(url, headers=header, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
-            return data['data'] ['subscribers']
+            return data['data']['subscribers']
 
         else:
             return 0
     except requests.exceptions.RequestException as e:
         return 0
-
