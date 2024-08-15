06b84e576a424f04b6739386d7184ba4b943e678
Dalhatmd <umarwalid4055@gmail.com>
2024-08-15 17:31:24 +0100

    Add task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to fix an issue with apache2 server
+
+
+exec { 'replace':
+  command => "sed -i 's/phpp/php/g' 'var/www/html/wp-settings.php",
+  path    => ['/bin', '/usr/bin/']
+}
