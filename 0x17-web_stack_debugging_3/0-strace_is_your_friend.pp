0fd1eef8fca396985d895ee764348d96ffacb688
Dalhatmd <umarwalid4055@gmail.com>
2024-08-15 22:36:49 +0100

    fix syntax error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 
 exec { 'replace':
-  command => "sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php",
+  command => "sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php'",
   path    => ['/bin', '/usr/bin']
 }
